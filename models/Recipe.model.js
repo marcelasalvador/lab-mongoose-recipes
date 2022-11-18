5c2633ca69c6e4c3f90e2c7df3a230657725f8bd
marcelasalvador <salvadormarcela18@gmail.com>
2022-11-17 23:32:17 -0600

    lab

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,6 +3,35 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: String,
+  level: {
+    type: String,
+    enum: [
+      "Easy Peasy", 
+      "Amateur Chef",
+      "UltraPro Chef"
+    ]
+  },
+  ingredients: [ String ],
+  cuisine: String,
+  dishType: {
+    type: String,
+    enum: [
+      "Breakfast", 
+      "main_course",
+      "Soup",
+      "Snack",
+      "Drink",
+      "dessert",
+      "Other"
+    ]
+  },
+  image: String,
+  duration: 0,
+  creator: String,
+  created: Date
+    
+
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
